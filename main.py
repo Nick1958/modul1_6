3f538cf5a9379d16c9cb77baef58fca66f60b961
NikolayTaybinov <taibinov@gmail.com>
2024-10-07 12:43:19 +0800

    Add files via upload

--- main.py
+++ main.py
@@ -1,5 +1,18 @@
-number_of_tasks=12
-number_of_hours=1.5
-title_of_course='Python'
-print(number_of_hours/number_of_tasks) # среднее время выполнения одного задания
-print(title_of_course, number_of_tasks, number_of_hours, number_of_hours/number_of_tasks)
+my_dict={'Alex':1948, 'Nick':1958, 'Dick':2004}
+print(my_dict)
+print(my_dict['Dick'])
+my_dict['Den']=2010
+print(my_dict)
+my_dict.update({'Bob':2008,
+                'Smit':1999})
+print(my_dict)
+a=my_dict.pop('Den')
+print(my_dict)
+print(a)
+
+my_set={1,1,2,3,4,2,3,'String',True,(1,2,7)}
+print(my_set)
+my_set.add(7)
+my_set.add(9)
+my_set.remove('String')
+print(my_set)
